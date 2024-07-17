14545d8945fee356df14ca9568e725322de06d07
stmilk <o3742058@yahoo.com.tw>
2024-07-17 20:13:47 +1000

    Convert absolute paths to relative paths.

--- tokenization/main.py
+++ tokenization/main.py
@@ -1,24 +1,23 @@
 import os
 from concurrent.futures import ProcessPoolExecutor
 
-# 数据集所在的目录
 data_dirs = [
-    'C:\\Users\\o3742\\Desktop\\literature-books-master\\folder1',
-    'C:\\Users\\o3742\\Desktop\\literature-books-master\\folder2',
-    'C:\\Users\\o3742\\Desktop\\literature-books-master\\folder3',
-    'C:\\Users\\o3742\\Desktop\\literature-books-master\\folder4',
-    'C:\\Users\\o3742\\Desktop\\literature-books-master\\folder5'
+    'folder1',
+    'folder2',
+    'folder3',
+    'folder4',
+    'folder5'
 ]
 
 output_dirs = [
-    'C:\\Users\\o3742\\Desktop\\literature-books-master\\tokenized_datasets_folder1',
-    'C:\\Users\\o3742\\Desktop\\literature-books-master\\tokenized_datasets_folder2',
-    'C:\\Users\\o3742\\Desktop\\literature-books-master\\tokenized_datasets_folder3',
-    'C:\\Users\\o3742\\Desktop\\literature-books-master\\tokenized_datasets_folder4',
-    'C:\\Users\\o3742\\Desktop\\literature-books-master\\tokenized_datasets_folder5'
+    'tokenized_datasets_folder1',
+    'tokenized_datasets_folder2',
+    'tokenized_datasets_folder3',
+    'tokenized_datasets_folder4',
+    'tokenized_datasets_folder5'
 ]
 
-script_path = 'C:\\Users\\o3742\\Desktop\\literature-books-master\\python\\process_folder.py'
+script_path = 'process_folder.py'
 
 def process_folder(data_dir, output_dir):
     os.system(f'python {script_path} {data_dir} {output_dir}')
@@ -28,6 +27,6 @@ if __name__ == "__main__":
         futures = [executor.submit(process_folder, data_dir, output_dir) for data_dir, output_dir in zip(data_dirs, output_dirs)]
         
         for future in futures:
-            future.result()  # 等待所有任務完成
+            future.result()  
 
     print("All folders processed and saved.")
